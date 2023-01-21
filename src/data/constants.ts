722d2102861db1fd13db0d6ffb048969c0a9c153
Ós <zeptok@gmail.com>
2023-01-21 16:56:10 -0600

    ✨ OG Image

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,5 +2,5 @@ export const Constants = {
   baseUrl: 'https://ocordova.me',
   name: 'Óscar Córdova',
   description: 'Product Manager',
-  ogImage: 'https://ocordova.me/og-image.jpg',
+  ogImage: 'https://ocordova.me/static/og-image.jpg',
 }
