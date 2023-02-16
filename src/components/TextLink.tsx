08e3bcd60d7065d0760251f3b5a3b16e1fb55b67
Ós <zeptok@gmail.com>
2023-02-15 21:11:17 -0600

    📝 Update about page

--- src/components/TextLink.tsx
+++ src/components/TextLink.tsx
@@ -1,10 +1,12 @@
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 
-export const TextLink = ({ href, newWindow, children }: any) => {
+export const TextLink = ({ href, newWindow, children, target, rel }: any) => {
   return (
     <Link
       href={href}
+      target={target}
+      rel={rel}
       {...(newWindow ? { target: '_blank', rel: 'noopener noreferrer' } : {})}
       className="bg-gradient-to-r from-purple-500 to-pink-500 bg-[length:0px_2px] bg-left-bottom bg-no-repeat pb-0.5 text-pink-500 transition-[background-size] duration-500 hover:bg-[length:100%_2px]"
     >
