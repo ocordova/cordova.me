5c7eea3eee8574c344b28189cd70c946204b5565
Ós <zeptok@gmail.com>
2023-02-15 23:16:58 -0600

    🔧 Set generateIndexSitemap false

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://ocordova.me',
-  generateRobotsTxt: true
+  generateRobotsTxt: true,
+  generateIndexSitemap: false
 }
