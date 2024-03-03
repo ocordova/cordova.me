0300c528c3fb785675c0f69f9a37cbe27dfe3d9a
Ós <zeptok@gmail.com>
2024-03-02 18:47:45 -0600

    📝 Update Home order

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Page() {
       <LatestThoughts />
       <Bookmarks />
       <Contact />
-      <NowReading />
       <NowListening />
+      <NowReading />
       <NowWatching />
     </>
   );
