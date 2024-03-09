f586a7c7330e04918e8e772e2240b71a9e5d363b
Ós <zeptok@gmail.com>
2024-03-09 14:28:16 -0600

    📝 Update work philosophy in introduction container

--- containers/introduction.tsx
+++ containers/introduction.tsx
@@ -9,13 +9,13 @@ const Introduction = () => {
       </section>
       <section className="mt-12">
         <h2 className="my-4 font-medium tracking-tight text-forground">
-          About
+          Work Philosophy
         </h2>
         <p className="text-sm leading-7 text-justify">
           I prioritize measurable outcomes over outputs, emphasizing long-term
-          sustainability over short-term profit. By directly engaging customers
-          and analyzing data, I ensure our solutions effectively address real
-          unmet needs.
+          sustainability over short-term profit. I engage directly with
+          customers and analyze data to ensure our solutions effectively address
+          real unmet needs.
         </p>
       </section>
     </>
