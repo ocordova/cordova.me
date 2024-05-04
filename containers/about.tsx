70ce4e08b0292f3062f92475b0924e5f2449e0cd
Ós <zeptok@gmail.com>
2024-05-03 18:51:05 -0600

    💄 Change text wrap

--- containers/about.tsx
+++ containers/about.tsx
@@ -10,7 +10,7 @@ const About = () => {
         <h2 className="my-4 font-medium tracking-tight text-forground">
           About
         </h2>
-        <p className="text-sm leading-7 text-justify">
+        <p className="text-sm leading-7 text-pretty">
           Part of the whole. Epicurean at heart. Product engineer. Cook.
           Defender of my mexican culinary heritage and passionate about
           preserving cultural traditions. Movie enthusiast. Human behavior
