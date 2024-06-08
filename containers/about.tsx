c2885c066c921b7a6b6441b46d3c98ee04cf929e
Ós <zeptok@gmail.com>
2024-06-08 02:11:41 -0600

    📝 Update about and now

--- containers/about.tsx
+++ containers/about.tsx
@@ -2,15 +2,10 @@ const About = () => {
   return (
     <>
       <section>
-        <div className="flex flex-col">
-          <h2 className="tracking-tight font-semibold">Óscar Córdova</h2>
-        </div>
-      </section>
-      <section className="mt-12">
         <h2 className="my-4 font-medium tracking-tight text-forground">
-          About
+          Óscar Córdova
         </h2>
-        <p className="text-sm leading-7 text-pretty">
+        <p className="text-sm leading-7">
           Part of the whole. Epicurean at heart. Product engineer. Cook.
           Defender of my mexican culinary heritage and passionate about
           preserving cultural traditions. Movie enthusiast. Human behavior
--- containers/now.tsx
+++ containers/now.tsx
@@ -6,7 +6,16 @@ const Now = () => {
         <p className="text-sm leading-7">
           I work as a Software Engineer by day at{" "}
           <span className="font-medium">________</span> and as a Product Manager
-          by night at <span className="font-medium">Artiflora</span>.
+          by night at{" "}
+          <a
+            className="underline-offset-4 underline hover:no-underline transition-all duration-200"
+            href="https://artiflora.mx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Artiflora
+          </a>
+          .
         </p>
       </section>
     </>
