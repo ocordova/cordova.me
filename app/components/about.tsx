20f622ea36e7a647b597cd9bacaf0ca48f2a0d8d
Ós <zeptok@gmail.com>
2024-09-13 19:07:25 -0600

    📝 Update about

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -7,9 +7,8 @@ const About = () => {
         </h2>
         <p className="text-sm leading-7">
           Part of the whole. Epicurean at heart. Product engineer. Cook.
-          Defender of my mexican culinary heritage and passionate about
-          preserving cultural traditions. Movie enthusiast. Human behavior
-          student.
+          Defender of my Mexican culinary heritage. Passionate about preserving
+          cultural traditions. Movie enthusiast. Student of human behavior.
         </p>
       </section>
     </>
