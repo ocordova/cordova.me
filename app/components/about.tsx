99113b179d7461be05add38fd6e9b6215b7cb1e9
Ós <zeptok@gmail.com>
2026-01-09 12:31:56 -0600

    ✨ Update About bio with refined expression

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -6,9 +6,9 @@ const About = () => {
           Óscar Córdova
         </h2>
         <p className="text-sm leading-7">
-          Part of the whole. Epicurean at heart, guided by the Tao. Product
-          engineer and cook. Keeper of Mexican culinary heritage. Seeker of
-          balance.
+          Part of the whole. A brief expression of the Tao. Unfolding as a
+          product engineer and cook. Keeper of Mexican culinary heritage. Seeker
+          of balance.
         </p>
       </section>
     </>
