303d71dc7b5d9f4079d44c0b3fe1e9e50a3afa0e
Ós <zeptok@gmail.com>
2026-02-06 18:15:02 -0600

    Update quotes.ts

--- app/db/quotes.ts
+++ app/db/quotes.ts
@@ -1,9 +1,9 @@
 export const quotes: string[] = [
-  "“To attain knowledge, add things everyday. To attain wisdom, remove things every day.” - Lao Tzu",
-  "“He who is not satisfied with a little, is satisfied with nothing.” - Epicurus",
-  "“A good traveler has no fixed plans and is not intent on arriving.” - Lao Tzu",
-  ""Do not spoil what you have by desiring what you have not." - Epicurus",
-  ""He who defines himself can't know who he really is." - Lao Tzu",
-  ""Trying to define yourself is like trying to bite your own teeth." - Alan Watts",
-  ""In Zen, we don't find the answers. We lose the questions." - Zen proverb",
+  "\u201CTo attain knowledge, add things everyday. To attain wisdom, remove things every day.\u201D - Lao Tzu",
+  "\u201CHe who is not satisfied with a little, is satisfied with nothing.\u201D - Epicurus",
+  "\u201CA good traveler has no fixed plans and is not intent on arriving.\u201D - Lao Tzu",
+  "\u201CDo not spoil what you have by desiring what you have not.\u201D - Epicurus",
+  "\u201CHe who defines himself can\u2019t know who he really is.\u201D - Lao Tzu",
+  "\u201CTrying to define yourself is like trying to bite your own teeth.\u201D - Alan Watts",
+  "\u201CIn Zen, we don\u2019t find the answers. We lose the questions.\u201D - Zen proverb",
 ];
