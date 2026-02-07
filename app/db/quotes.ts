8d6baca82f56326dd6b8789d5beda639e8466f43
Ós <zeptok@gmail.com>
2026-02-06 18:13:02 -0600

    ✨ Add new quotes from Lao Tzu, Alan Watts, and Zen proverb

--- app/db/quotes.ts
+++ app/db/quotes.ts
@@ -2,5 +2,8 @@ export const quotes: string[] = [
   "“To attain knowledge, add things everyday. To attain wisdom, remove things every day.” - Lao Tzu",
   "“He who is not satisfied with a little, is satisfied with nothing.” - Epicurus",
   "“A good traveler has no fixed plans and is not intent on arriving.” - Lao Tzu",
-  "“Do not spoil what you have by desiring what you have not.” - Epicurus",
+  ""Do not spoil what you have by desiring what you have not." - Epicurus",
+  ""He who defines himself can't know who he really is." - Lao Tzu",
+  ""Trying to define yourself is like trying to bite your own teeth." - Alan Watts",
+  ""In Zen, we don't find the answers. We lose the questions." - Zen proverb",
 ];
